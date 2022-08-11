4c00bfecee1a8151421b1707c4e88560a949b2da
EmmyMay <lekeemmy@gmail.com>
2022-08-11 13:50:53 +0100

    created label

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ function inputInteger(options) {
     theme,
     label = "Input Integer",
     inputContainerClass = "input_container",
+    inputId = "Input-Integer",
   } = options;
 
   const styleSheet = new CSSStyleSheet();
@@ -14,14 +15,19 @@ function inputInteger(options) {
   el.setAttribute("id", "input_wrapper");
 
   const shadow = el.attachShadow({ mode: "closed" });
+
   const input = document.createElement("input");
   input.type = "number";
   input.min = min;
   input.max = max;
+  input.setAttribute("id", inputId);
+  const inputLabel = document.createElement("label");
+  inputLabel.setAttribute("for", inputId);
+  inputLabel.textContent = label;
 
   const inputContainer = document.createElement("div");
   inputContainer.setAttribute("class", inputContainerClass);
-  inputContainer.appendChild(input);
+  inputContainer.append(inputLabel, input);
   shadow.appendChild(inputContainer);
 
   shadow.adoptedStyleSheets = [styleSheet];
