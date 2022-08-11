08f5e879ef716f1145815e625e9094c7ea8e523b
EmmyMay <lekeemmy@gmail.com>
2022-08-11 17:40:30 +0100

    included new options properties; included event handler

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 function inputInteger(options) {
   const {
-    min = 1,
+    min = 0,
     max = 1000,
     theme,
     label = "Input Integer",
     inputContainerClass = "input_container",
     inputId = "Input-Integer",
+    step = "0",
   } = options;
 
   const styleSheet = new CSSStyleSheet();
@@ -20,7 +21,10 @@ function inputInteger(options) {
   input.type = "number";
   input.min = min;
   input.max = max;
+  input.step = step;
   input.setAttribute("id", inputId);
+  input.onkeyup = (e) => handle_onkeyup(e, input);
+
   const inputLabel = document.createElement("label");
   inputLabel.setAttribute("for", inputId);
   inputLabel.textContent = label;
@@ -32,7 +36,12 @@ function inputInteger(options) {
 
   shadow.adoptedStyleSheets = [styleSheet];
 
-  return el;
+  return { el, input };
 }
 
+function handle_onkeyup(e, input) {
+  let value = Number(e.target.value);
+  if (value > input.max) input.value = input.max;
+  if (value < input.min) input.value = 0;
+}
 module.exports = inputInteger;
