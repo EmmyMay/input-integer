fc5592df9113158df3d511ef914231d22501a704
EmmyMay <lekeemmy@gmail.com>
2022-08-11 12:40:54 +0100

    render an input integer in the dom

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-const inputInteger = (name = "Jack Harlow") => {
-  return name
+function inputInteger(options) {
+  const { min = 1, max = 1000, theme, label = "Input Integer" } = options;
+
+  const styleSheet = new CSSStyleSheet();
+  styleSheet.replaceSync(theme);
+
+  const el = document.createElement("div");
+  el.setAttribute("id", "input_wrapper");
+
+  const shadow = el.attachShadow({ mode: "closed" });
+  const input = document.createElement("input");
+  input.type = "number";
+  input.min = min;
+  input.max = max;
+
+  const inputContainer = document.createElement("div");
+  inputContainer.setAttribute("class", "input_container");
+  inputContainer.appendChild(input);
+  shadow.appendChild(inputContainer);
+
+  shadow.adoptedStyleSheets = [styleSheet];
+
+  return el;
 }
 
-
-module.exports = inputInteger
+module.exports = inputInteger;
