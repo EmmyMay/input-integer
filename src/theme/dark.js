98a16bc42c8166c551f0bd0b64532ee561cebe31
EmmyMay <lekeemmy@gmail.com>
2022-08-28 03:01:19 +0100

    updated the styling to use the host selector

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -1,13 +1,13 @@
 function theme(containerClass) {
   return `
-  input {
+:host input {
   padding: 1rem;
   background-color: hsla(0, 0%, 4%, 0.205);
   border: none;
   width: 15rem;
 }
 
-.${containerClass} {
+:host div {
   position: relative;
   width: fit-content;
   display: flex;
@@ -15,7 +15,7 @@ function theme(containerClass) {
   gap: 1rem;
 }
 
-.${containerClass}::after {
+:host div::after {
   content: '';
   position: absolute;
   width: 100%;
@@ -28,22 +28,22 @@ function theme(containerClass) {
   transition: transform 200ms ease-in;
   z-index: -1;
 }
-input:focus {
+:host input:focus {
   outline: none;
 
 }
 
-.${containerClass}:focus-within::after {
+:host div:focus-within::after {
   transform: scaleX(1);
   transform-origin: left;
 }
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
+:host input::-webkit-outer-spin-button,
+:host input::-webkit-inner-spin-button {
     /* display: none; <- Crashes Chrome on hover */
     -webkit-appearance: none;
     margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
 }
-input[type=number] {
+:host input[type=number] {
     -moz-appearance:textfield; /* Firefox */
 }
 
--- src/theme/light.js
+++ src/theme/light.js
@@ -1,13 +1,13 @@
 function theme(containerClass) {
   return `
-  input {
+:host input {
   padding: 1rem;
   background-color: hsla(35, 88%, 94%, 0.397);
   border: none;
   width: 15rem;
 }
 
-.${containerClass} {
+:host div {
   position: relative;
   width: fit-content;
   display: flex;
@@ -15,7 +15,7 @@ function theme(containerClass) {
   gap: 1rem;
 }
 
-.${containerClass}::after {
+:host div::after {
   content: '';
   position: absolute;
   width: 100%;
@@ -28,25 +28,25 @@ function theme(containerClass) {
   transition: transform 200ms ease-in;
   z-index: -1;
 }
-input:focus {
+:host input:focus {
   outline: none;
   box-shadow: -4px 7px 20px -5px rgba(0,0,0,0.29);
 -webkit-box-shadow: -4px 7px 20px -5px rgba(0,0,0,0.29);
 -moz-box-shadow: -4px 7px 20px -5px rgba(0,0,0,0.29);
 }
 
-.${containerClass}:focus-within::after {
+:host div:focus-within::after {
   transform: scaleX(1);
   transform-origin: left;
 }
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
+:host input::-webkit-outer-spin-button,
+:host input::-webkit-inner-spin-button {
     /* display: none; <- Crashes Chrome on hover */
     -webkit-appearance: none;
     margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
 }
 
-input[type=number] {
+:host input[type=number] {
     -moz-appearance:textfield; /* Firefox */
 }
 
